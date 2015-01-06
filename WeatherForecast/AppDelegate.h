1d80b47b3170fe2af680c0de67b2ca1426211d74
Adenike Olatunji <GloryOlatunji@Adenikes-MacBook-Pro.local>
2015-01-06 05:10:27 +0000

    Initial Commit

---
+++ WeatherForecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForecast
+//
+//  Created by Adenike Olatunji on 06/01/2015.
+//  Copyright (c) 2015 Adenike Olatunji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherForecast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherForecast
+//
+//  Created by Adenike Olatunji on 06/01/2015.
+//  Copyright (c) 2015 Adenike Olatunji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
